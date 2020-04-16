e2fc4f80742ebce6122bece7beba0171d927e91d
dilya <dilya.anvarbekova@utexas.edu>
2020-04-16 18:09:17 -0500

    Added Deck class
    Updated go_fish.cpp with Deck class testing.

---
+++ deck.cpp
@@ -0,0 +1,58 @@
+/*
+ * deck.cpp
+ * Teddy Hsieh and Dilya Anvarbekova
+ *
+ */
+
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <time.h>
+#include "deck.h"
+
+using namespace std;
+
+Deck::Deck(){
+    int rank = 1;
+    Card::Suit suit = Card::spades;
+    myIndex = 0;
+    for(int i = 0; i < SIZE; i++){
+        if(i == 13){
+            suit = Card::diamonds;
+        }
+        else if(i == 26){
+            suit = Card::clubs;
+        }
+        else if(i == 39){
+            suit = Card::hearts;
+        }
+        myCards[i] = Card(rank, suit);
+        rank = (rank+1) % 14;
+        if(rank == 0){
+            rank = 1;
+        }
+    }
+}
+
+int Deck::size() const {
+    int deckSize = 52 - (myIndex);
+    return deckSize;
+}
+
+Card Deck::dealCard() {
+    Card deal = myCards[myIndex];
+    myIndex += 1;
+    return deal;
+}
+
+void Deck::shuffle() {
+    myIndex = 0;
+    Card temp;
+    srand(time(0));
+    for(int i = 0; i < SIZE - 1; i++){
+        int j = rand() % (SIZE - i) + i;
+        temp = myCards[i];
+        myCards[i] = myCards[j];
+        myCards[j] = temp;
+    }
+}
--- go_fish.cpp
+++ go_fish.cpp
@@ -10,7 +10,7 @@ using namespace std;
 
 int main(){
 
-    cout << "Making a bunch of cards..." << endl;
+    cout << "Testing Card class..." << endl;
     Card c0 = Card();
     Card c1 = Card(5, Card::hearts);
     Card c2 = Card(11, Card::diamonds);
@@ -33,6 +33,20 @@ int main(){
     cout << "Are card 1 and card 6 the same suit? " << c0.sameSuitAs(c5) << endl;
     cout << "Are card 2 and card 4 the same suit? " << c1.sameSuitAs(c3) << endl;
 
-    cout << "Card 1 rank: " << c0.rankString(c0.getRank()) << endl;
+    cout << "Card 1 rank: " << c0.rankString(c0.getRank()) << endl << endl;
 
+    cout << "Testing Deck class..." << endl;
+    Deck d0 = Deck();
+    cout << "Deck size is " << d0.size() << endl;
+    cout << "Dealing the sorted deck..." << endl;
+    for(int i = 0; i < 52; i++){
+        cout << "Card: " << d0.dealCard().toString() << " Num cards left: " << d0.size() << endl;
+    }
+
+    cout << endl;
+    cout << "Shuffle cards..." << endl;
+    d0.shuffle();
+    for(int i = 0; i < 52; i++){
+        cout << "Card: " << d0.dealCard().toString() << " Num cards left: " << d0.size() << endl;
+    }
 }
